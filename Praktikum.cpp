89a020c2d264e61cd3ae7d3dc05cda1871183e8c
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-03-03 15:34:45 +0700

    prosedur dan fungsi

--- Praktikum.cpp
+++ Praktikum.cpp
@@ -1,4 +1,14 @@
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 // Deklarasi Global
+    float p,l;
+
+//Implementasi Prosedur & Fungsi
+void input() {
+    cout << "Masukkan sisi Panjang : ";
+    cin >> p;
+    cout << "Masukkan sisi Lebar : ";
+    cin >> l;
+}
+  
