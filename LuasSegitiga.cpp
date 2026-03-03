57bc870f49355bcacdad5f0a5c032fb25b765e59
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-03-03 16:19:20 +0700

    Implement input function

--- LuasSegitiga.cpp
+++ LuasSegitiga.cpp
@@ -4,3 +4,17 @@ using namespace std;
 float hitungLuas(float a, float t) {
     return 0.5 * a * t;
 }
+
+void input() {
+    double a, t, luas;
+
+    cout << "Masukkan alas   : ";
+    cin >> a;
+
+    cout << "Masukkan tinggi : ";
+    cin >> t;
+
+    luas = hitungLuas(a, t);
+
+    cout << "Luas segitiga = " << luas << endl;
+}
